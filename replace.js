4e072276080ba8c32e36038ba34dd866e8a4eee7
Sammy Miyan <mrbunny045@gmail.com>
2019-10-30 21:39:23 +0530

    replace any word within the counter limit

---
+++ replace.js
@@ -0,0 +1,14 @@
+'use strict';
+function repx(rval,val,rep,count){
+	if(time!=null){
+		for(var i=0;i<count;i++){
+			rval=rval.replace(val,rep);
+		};
+		return rval;
+	}else{
+		while(rval.match(val)!=null){
+			rval=rval.replace(val,rep);
+		};
+		return rval;
+	};
+};
